ff890d4cad6daf68ac9f2a0e6ee413c8ba006d7c
Marcus Amaral Rolim <m4rkux@gmail.com>
2022-07-06 02:34:03 -0700

    Update unit tests

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -2,15 +2,35 @@ import { describe, it, expect } from "vitest";
 
 import { mount } from "@vue/test-utils";
 import HelloWorld from "../HelloWorld.vue";
+import { createI18n } from "vue-i18n";
+import { enUS, ptBR } from "@/locales";
+
+const i18n = createI18n({
+  locale: "en-US",
+  messages: {
+    "en-US": enUS,
+    "pt-BR": ptBR,
+  },
+});
 
 describe("HelloWorld", () => {
   it("renders properly", () => {
-    const wrapper = mount(HelloWorld, { props: { msg: "Hello Vitest" } });
+    const wrapper = mount(HelloWorld, {
+      global: {
+        plugins: [i18n],
+      },
+      props: { msg: "Hello Vitest" },
+    });
     expect(wrapper.text()).toContain("Hello Vitest");
   });
 
   it("renders properly 2", () => {
-    const wrapper = mount(HelloWorld, { props: { msg: "Hello Vitest 2" } });
+    const wrapper = mount(HelloWorld, {
+      global: {
+        plugins: [i18n],
+      },
+      props: { msg: "Hello Vitest 2" },
+    });
     expect(wrapper.text()).toContain("Hello Vitest 2");
   });
 });
--- src/components/__tests__/TheWelcome.spec.ts
+++ src/components/__tests__/TheWelcome.spec.ts
@@ -1,11 +1,17 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
+import { createTestingPinia } from "@pinia/testing";
 
 import { mount } from "@vue/test-utils";
 import TheWelcome from "../TheWelcome.vue";
 
 describe("The Welcome", () => {
   it("renders properly", () => {
-    const wrapper = mount(TheWelcome);
-    expect(wrapper.text()).toContain("Documentation");
+    const wrapper = mount(TheWelcome, {
+      global: {
+        plugins: [createTestingPinia({ createSpy: vi.fn })],
+      },
+    });
+
+    expect(wrapper.text()).toContain("Pinia");
   });
 });
